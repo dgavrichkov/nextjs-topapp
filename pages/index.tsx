f7a201e9cf802142e22ed6b8b66990fba4e86581
ditmar <dgavrichkov32@gmail.com>
2023-01-16 10:13:01 +0300

    clear home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,7 @@
-import { Button, HTag, Par, Tag } from '../components';
-
 export default function Home(): JSX.Element {
 	return (
 		<div>
-			<HTag tag='h1'>ASS</HTag>
-			<Button appearance='primary' arrow='right'>Button</Button>
-			<Button appearance='ghost' arrow='right'>Button ghost</Button>
-			<Par size="lg">paragraph lg</Par>
-			<Par size="md">paragraph md</Par>
-			<Par>paragraph sm</Par>
-			<Tag color='ghost' size='lg'>Tag 1</Tag>
-			<Tag color='green'>Tag 1</Tag>
-			<Tag color='red'>Tag 1</Tag>
-			<Tag color='grey'>Tag 1</Tag>
-			<Tag color='primary' href='https://sosibibu'>Tag 1</Tag>
+			Home Component
 		</div>
 	);
 }
