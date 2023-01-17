dce63ad5cfd9855da1609d4275e8c0480e866679
ditmar <dgavrichkov32@gmail.com>
2023-01-17 18:10:50 +0300

    добавлены запросы на сервер, статическая генерация страниц с продуктами

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,42 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
 import { Rating } from '../components';
 import { withLayout } from '../layouts';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 	const [rating, setRating] = useState(4);
 
 	return (
 		<>
 			Home Component
 			<Rating rating={rating} setRating={setRating} isEditable />
+			<ul>
+				{menu.map(m => (
+					<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+				))}
+			</ul>
 		</>
 	);
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory,
+		}
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
