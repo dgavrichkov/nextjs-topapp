6e807d0da9de049ca8f94b0a7ecdb0a30bde119f
dmitriy_gavrichkov <dmitriy_gavrichkov@senla.eu>
2023-01-28 12:16:16 +0300

    search component

--- components/index.ts
+++ components/index.ts
@@ -8,3 +8,4 @@ export * from './Advantages';
 export * from './Sort';
 export * from './Input';
 export * from './Textarea';
+export * from './Search';
--- layouts/Sidebar/Sidebar.tsx
+++ layouts/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-import { Menu } from '../../components';
+import { Menu, Search } from '../../components';
 import { SidebarProps } from './SidebarProps';
 import Logo from '../logo.svg';
 import styles from './Sidebar.module.scss';
@@ -8,7 +8,7 @@ export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 	return (
 		<div className={cn(className, styles.sidebar)} {...props}>
 			<Logo />
-			<div>search bar</div>
+			<Search />
 			<Menu />
 		</div>
 	);
